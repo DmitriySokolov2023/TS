0955a9da4b75bc552300713195690885655e978e
unknown <sokolovoff2023@gmail.com>
2023-12-18 20:42:01 +0300

    TS-1

---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FC} from "react";
+
+const EventsExample: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default EventsExample
+:
+FC;
